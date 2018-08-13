a712a998bd5418eedf30c06e708edaf621946819
Oliver de Sousa <oliver.seq@gmail.com>
2018-08-13 16:45:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 // write your code below!
 
 var name = "Olly"
-var height = "30"
+var age = "30"
 
 console.log("Name:"+name)
 
-console.log("Height:")
-console.log(height)
+console.log("Height:"+age)
